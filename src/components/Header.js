58ea2499a7a4cb099897fe5f276a8bfd7d6a7f02
AnhQuoc2903 <133018919+AnhQuoc2903@users.noreply.github.com>
2026-02-09 10:01:48 +0700

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import './Header.css';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Header.css";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const { t } = useTranslation();
@@ -16,22 +16,34 @@ const Header = () => {
         </div>
 
         {/* Nút menu trên mobile */}
-        <button
-          className="menu-toggle"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
+        <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
           ☰
         </button>
 
         {/* Menu chính */}
-        <nav className={`main-nav ${menuOpen ? 'open' : ''}`}>
+        <nav className={`main-nav ${menuOpen ? "open" : ""}`}>
           <ul>
-            <li><Link to="/trang-chu">{t('header.home')}</Link></li>
-            <li><Link to="/ve-chung-toi">{t('header.about')}</Link></li>
-            <li><Link to="/linh-vuc">{t('header.services')}</Link></li>
-            <li><Link to="/tin-tuc">{t('header.news')}</Link></li>
-            <li><Link to="/tuyen-dung">{t('header.recruitment')}</Link></li>
-            <li><Link to="/lien-he">{t('header.contact')}</Link></li>
+            <li>
+              <Link to="/trang-chu">{t("header.home")}</Link>
+            </li>
+            <li>
+              <Link to="/ve-chung-toi">{t("header.about")}</Link>
+            </li>
+            <li>
+              <Link to="/linh-vuc">{t("header.services")}</Link>
+            </li>
+            <li>
+              <Link to="/tin-tuc">{t("header.news")}</Link>
+            </li>
+            <li>
+              <Link to="/tuyen-dung">{t("header.recruitment")}</Link>
+            </li>
+            <li>
+              <Link to="/lien-he">{t("header.contact")}</Link>
+            </li>
+            <li>
+              <Link to="/login">{t("header.login")}</Link>
+            </li>
           </ul>
         </nav>
       </div>
